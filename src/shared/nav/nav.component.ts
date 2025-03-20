0125b683bcb7b62a9d9fe3949cd502bef3ce1f38
Ivan Ivanov <ivanivamar99@gmail.com>
2025-03-20 17:35:27 +0100

    init

---
+++ src/shared/nav/nav.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-nav',
+    imports: [
+        NgOptimizedImage
+    ],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.sass'
+})
+export class NavComponent {
+
+}
