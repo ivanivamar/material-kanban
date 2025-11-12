d959dcbdf41a7fd2c1f27cdaed06caf344549319
Ivan Ivanov <ivanivamar99@gmail.com>
2025-11-12 12:50:07 +0100

    Improves project setup and UI
    Increases the default indentation size in the editor configuration.
    
    Adds an asset configuration for /src/assets to include the shapes.
    
    Sets up basic UI elements, including a loader and prepares project for future components.
    Removes placeholder content from the main app component.
    
    Adds primeflex css file, and styles file.

---
+++ src/app/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  imports: [],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.sass'
+})
+export class LoaderComponent {
+}
